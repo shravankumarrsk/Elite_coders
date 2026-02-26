355c01356d580ceb906a2ba66aea1ce9aa932399
shravankumarrsk <shravankumarrsk>
2026-02-26 10:25:48 +0530

    day 3...3 sum problem

---
+++ 3.cpp
@@ -0,0 +1,44 @@
+/* Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+
+
+*/
+
+//optimal solution
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+       int n=nums.size();
+       sort(nums.begin(),nums.end());
+       vector<vector<int>>ans;
+      
+       for(int i=0;i<n;i++){
+        if(i>0 && nums[i]==nums[i-1]) continue;
+        int j=i+1,k=n-1;
+        while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum>0){
+                k--;
+            } 
+            else if(sum<0){
+                j++;
+            }
+            else{
+                ans.push_back({nums[i],nums[j],nums[k]});
+                j++,k--;
+                while(j<k && nums[j]==nums[j-1])
+                j++;
+            }
+        }
+       }
+       return ans;
+    }
+};
