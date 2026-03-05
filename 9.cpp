b92ef219c8e1fceb5a3a9636b6b3f2eb5bb1f206
shravankumarrsk <shravankumarrsk>
2026-03-05 22:41:18 +0530

    day 9

---
+++ 9.cpp
@@ -0,0 +1,36 @@
+/*Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+
+
+
+subarray sum equals K
+
+
+*/
+
+//bruteforce appraoch
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+       int n=nums.size();
+       int count=0;
+       for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+            sum+=nums[j];
+            if(sum==k) count++;
+        }
+       } 
+       return count;
+    }
+};
