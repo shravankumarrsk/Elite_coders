4879d5940fa964c0dabc1329b22f6b8b22bec812
shravankumarrsk <shravankumarrsk>
2026-03-07 00:15:36 +0530

    day 10

---
+++ 10.cpp
@@ -0,0 +1,43 @@
+/*Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+
+
+longest consecutive sequence
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size();
+        if(nums.size()==0) return 0;
+        sort(nums.begin(),nums.end());
+        int count=1;
+        int longest=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]) continue;
+            
+        if(nums[i]==nums[i-1]+1){
+            count++;
+        }else{
+            longest=max(longest,count);
+            count=1;
+        }
+        }
+        return max(longest,count);
+    }
+};
